9e58477782a134ca85dd69423a2cb3c79c337eb0
Arnaud Bergeron <abergeron@gmail.com>
2015-04-29 15:36:13 -0400

    Stop hiding error messages with errors about constant copying.

--- theano/sandbox/gpuarray/type.py
+++ theano/sandbox/gpuarray/type.py
@@ -296,8 +296,12 @@ class GpuArrayConstant(_operators, Constant):
     def __str__(self):
         if self.name is not None:
             return self.name
-        return "GpuArrayConstant{%s}" % numpy.asarray(self.data)
-
+        try:
+            np_data = numpy.asarray(self.data)
+        except gpuarray.GpuArrayException:
+            np_data = self.data
+        return "GpuArrayConstant{%s}" % np_data
+ù
 
 GpuArrayType.Constant = GpuArrayConstant
 
